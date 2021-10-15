519ede592b5a1854deb7d016af854ae73296b3d1
ZOTTCE <zottce@gmail.com>
2021-10-15 16:44:30 +0300

    update client builder

--- client/build.rs
+++ client/build.rs
@@ -1,4 +1,9 @@
+const DIRECTX_DEFAULT_PATH: &str =
+    "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86";
+
 fn main() {
-    println!("cargo:rustc-link-search=native=C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86");
+    let directx_sdk = std::env::var("DX_SDK").unwrap_or(DIRECTX_DEFAULT_PATH.to_string());
+
+    println!("cargo:rustc-link-search=native={}", directx_sdk);
     println!("cargo:rustc-link-lib=static=d3dx9");
 }
